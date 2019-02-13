523b98d4136f3dd692299395339cfb9b64530f6c
liuqi2016 <1551255297@qq.com>
2019-02-13 17:32:34 +0800

    git

--- blog57/resources/views/welcome.blade.php
+++ blog57/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    blog2
+                    blog3
                 </div>
 
                 <div class="links">
